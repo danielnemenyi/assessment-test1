8b0fb33be891b975e350add9c42c7173789c0de7
Daniel Nemenyi <danielsu@pompo.co>
2020-06-29 17:42:42 +0100

    Initial commit of test assessment file
    * prog1.py

---
+++ prog1.py
@@ -0,0 +1,20 @@
+# This is a test assessment. Just seeing how it all works...
+
+# Here's a
+
+print('Hello World')
+
+# Something is wrong with this. Please fix.
+
+prin('Hello World')
+
+# What's the matter with this function?
+
+def manymany(word, num):
+    print word * num
+
+# Now run manymany
+
+
+
+
